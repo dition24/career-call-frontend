eefea56addd85668bcbe7d357b0a671d9ea0d106
Daniel Placencia <Dition24@Gmail.com>
2023-03-04 17:52:31 -0500

    adds select

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,9 +1,20 @@
 //right details
+import { react } from "react";
+import Index from "./Index";
 
 function Select(props) {
-    return (
-        <h1></h1>
-    )
+    const loaded = () => {
+        const [ post, setPost ] = useState (null);
+        return (
+            <div>{post}</div>
+        )
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return props.posts ? loaded() : loading();
 }
 
 export default Select
