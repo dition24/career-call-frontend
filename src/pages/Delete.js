8d667be632639ee29993286fe852bbf620bbeea9
Logan <logkauf14@gmail.com>
2023-03-11 16:59:00 -0500

    changes to delete pages

--- src/pages/Delete.js
+++ src/pages/Delete.js
@@ -1,15 +1,30 @@
 // edit/Delete page for employers
-
+import { useState } from 'react'
 import Nav from '../components/Nav'
 import Index from '../components/Index'
 import DeleteSelect from '../components/DeleteSelect'
 
 function Delete(props) {
+   
+    const [career, setCareer] = useState(null)
+    
+    const URL = 'http://localhost:3001/hires'
+
+    const getCareer = async () => {
+        try {
+            const response = await fetch(URL)
+            const data = await response.json()
+            setCareer(data)
+        } catch (error) {
+            console.log('error performing task')
+        }
+    }
+
     return (
         <div>
             <Nav />
             <Index />
-            {/* <DeleteSelect title= location= description= company= />  */}
+            <DeleteSelect title='who' location='where' description='what' company='apple' /> 
             {/* pass props to DeleteSelect */}
         </div>
     )
